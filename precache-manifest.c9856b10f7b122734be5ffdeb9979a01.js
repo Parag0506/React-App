6f54719c7ff60e27a8370b3c90634dae19c1c02c
Parag0506 <paragghorpade1998@gmail.com>
2020-07-31 13:58:50 +0530

    App-deploy

---
+++ precache-manifest.c9856b10f7b122734be5ffdeb9979a01.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03830c53eaceb7db7a106f943166f49",
+    "url": "/black-dashboard-react/index.html"
+  },
+  {
+    "revision": "8ceb9a6d75d0206a51f0",
+    "url": "/black-dashboard-react/static/css/main.d4360377.chunk.css"
+  },
+  {
+    "revision": "4a1439720e893d3fd003",
+    "url": "/black-dashboard-react/static/js/2.835233da.chunk.js"
+  },
+  {
+    "revision": "5f2ac350033ab5c4bebfa6b5635c1ace",
+    "url": "/black-dashboard-react/static/js/2.835233da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ceb9a6d75d0206a51f0",
+    "url": "/black-dashboard-react/static/js/main.fcf91c96.chunk.js"
+  },
+  {
+    "revision": "67d2f5ba5143fe1affa48e55357bf584",
+    "url": "/black-dashboard-react/static/js/main.fcf91c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d52a693be451dff251",
+    "url": "/black-dashboard-react/static/js/runtime-main.d73f8090.js"
+  },
+  {
+    "revision": "346988f13fdbe86fd3ef9b183bf2d6bb",
+    "url": "/black-dashboard-react/static/media/akash.346988f1.jpeg"
+  },
+  {
+    "revision": "e5a3de3ea09fa5328a6c3f0eee906334",
+    "url": "/black-dashboard-react/static/media/mane.e5a3de3e.jpeg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "237ce3b34a07413a4891117b1b212b23",
+    "url": "/black-dashboard-react/static/media/parag.237ce3b3.jpeg"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/black-dashboard-react/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "37c38062812eb1ee3a02fe951ea652b6",
+    "url": "/black-dashboard-react/static/media/richa.37c38062.jpg"
+  },
+  {
+    "revision": "e5e22dab1205de3509a53524817222e1",
+    "url": "/black-dashboard-react/static/media/sakshi.e5e22dab.jpeg"
+  },
+  {
+    "revision": "43b330065e02214dbad23701cdba293c",
+    "url": "/black-dashboard-react/static/media/sih-logo.43b33006.png"
+  },
+  {
+    "revision": "69942665ad39e492c61375577623bbf6",
+    "url": "/black-dashboard-react/static/media/varun.69942665.jpeg"
+  }
+]);
